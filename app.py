2f3ff304b63ac7b28291862d20683da8e590ff2f
Takeda Taiki <take5.tai412@gmail.com>
2024-11-14 17:30:56 +0900

    Update todo

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 
-@app.route('/')
-def hello():
-    return 'Hello, Render!'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(100), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        task = request.form['task']
+        new_task = Todo(task=task)
+        db.session.add(new_task)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    tasks = Todo.query.all()
+
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def task_delete(id):
+    task = Todo.query.get_or_404(id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
